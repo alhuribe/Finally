f764fbdbb564235b76ed5a33e1c6fb5e9b9050c7
alhuribe <yaf3.org@gmail.com>
2025-04-25 06:04:12 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.post('/g', (req, res) => {
+  console.log('Received event:', req.body);
+  res.status(200).send('Event received');
+});
+
+app.get('/', (req, res) => {
+  res.send('GTM Server Container is running.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
